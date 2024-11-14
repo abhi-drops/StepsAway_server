b78a120d042f21522b4f0c4f4dd94ea49295b6fd
abhi-drops <abhinand7667@gmail.com>
2024-11-14 23:11:10 +0530

    final 2.2

--- DB/connection.js
+++ DB/connection.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose')
-const connectionString  = process.env.connectionString
+const CONNECTIONSTRING  = process.env.CONNECTIONSTRING
 
-mongoose.connect(connectionString).then(()=>{
+mongoose.connect(CONNECTIONSTRING).then(()=>{
   console.log("MongoDB Atlas successfully connected to Steps Away Server");
 
 
 }).catch((err)=>{
   console.log("mongodb connection failed!!!",err);
 })
-
